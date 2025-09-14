eedded85f650acb078966172e20110222886277b
Karema Hamdy <131587176+karemahamdy@users.noreply.github.com>
2025-09-14 15:01:27 +0300

    [1204] [Time Beats: 40.31%] - LeetPush

---
+++ Last-Person-to-Fit-in-the-Bus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+WITH sum_weights AS (
+    SELECT *,
+    SUM(weight) OVER(ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM sum_weights 
+WHERE total_weight<=1000
+ORDER BY total_weight DESC
+LIMIT 1
